d53af996ccfae581343b7374fccf85812673c911
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-02-22 05:22:55 -0600

    JS updates and adding about me section

--- app.js
+++ app.js
@@ -1,50 +1,36 @@
-let ham = document.querySelector(".hamburger");
-let nav = document.querySelector(".topnav");
-// let third = document.querySelector(".third-container");
-let skillsNav = document.querySelector(".skills-nav");
-let skills = document.querySelector(".skill-container");
-let portNav = document.querySelector(".port-nav");
-let portfolio = document.querySelector(".second-container");
+const ham = document.querySelector(".hamburger");
+const nav = document.querySelector(".topnav");
+const third = document.querySelector(".third-container");
+const skillsNav = document.querySelector(".skills-nav");
+const skills = document.querySelector(".skill-container");
+const portNav = document.querySelector(".port-nav");
+const portfolio = document.querySelector(".second-container");
 
+const homeNav = document.querySelector(".home-nav");
+const home = document.querySelector(".first-container");
 
-let homeNav = document.querySelector(".home-nav");
-let home = document.querySelector(".first-container");
+const meetBtn = document.querySelector(".meet-btn");
 
-ham.addEventListener("click", function(){
-    nav.classList.toggle("active1");
+ham.addEventListener("click", function () {
+  nav.classList.toggle("active1");
 });
 
-skillsNav.addEventListener("click", function()  {
-    skills.classList.add("active");
-    nav.classList.remove("active1");
-
+skillsNav.addEventListener("click", function () {
+  skills.classList.add("active");
+  nav.classList.remove("active1");
 });
 
-portNav.addEventListener("click", function()  {
-    portfolio.classList.add("active");
-    nav.classList.remove("active1");
-
+portNav.addEventListener("click", function () {
+  portfolio.classList.add("active");
+  nav.classList.remove("active1");
 });
 
-homeNav.addEventListener("click", function()  {
-   home.classList.add("active");
-    nav.classList.remove("active1");
-
+homeNav.addEventListener("click", function () {
+  home.classList.add("active");
+  nav.classList.remove("active1");
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+meetBtn.addEventListener("click", function () {
+  home.style.display = "none";
+  third.style.display = "block";
+});
