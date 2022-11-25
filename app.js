7f1e4c0448971f1f4b2c63811658d167b8e1108c
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-11-24 20:44:34 -0600

    new updates to site

--- app.js
+++ app.js
@@ -16,37 +16,101 @@ const meetBtn = document.querySelector(".meet-btn");
 
 const listItem = document.querySelectorAll(".list-item");
 
+const workHistory = document.querySelectorAll(".single-work-history");
+
+const mobileNav = document.querySelectorAll(".mobile-nav");
+
+mobileNav.forEach((item, i, arr) => {
+  item.addEventListener("click", () => {
+    nav.classList.toggle("active1");
+  });
+});
+
 ham.addEventListener("click", function () {
   nav.classList.toggle("active1");
 });
 
-skillsNav.addEventListener("click", function () {
-  header.classList.add("active3");
-  skills.classList.add("active2");
-  nav.classList.remove("active1");
-});
+// skillsNav.addEventListener("click", function () {
+//   header.classList.add("active3");
+//   skills.classList.add("active2");
+//   nav.classList.remove("active1");
+// });
 
-portNav.addEventListener("click", function () {
-  portfolio.classList.add("active");
-  nav.classList.remove("active1");
-});
+// portNav.addEventListener("click", function () {
+//   portfolio.classList.add("active");
+//   nav.classList.remove("active1");
+// });
 
-homeNav.addEventListener("click", function () {
-  header.classList.remove("active3");
-  skills.classList.remove("active2");
-  nav.classList.remove("active1");
-});
+// homeNav.addEventListener("click", function () {
+//   header.classList.remove("active3");
+//   skills.classList.remove("active2");
+//   nav.classList.remove("active1");
+// });
 
 meetBtn.addEventListener("click", function () {
   header.style.display = "none";
   skills.classList.add("active2");
 });
 
-listItem.forEach((list, i) => {
-  list.addEventListener("click", function () {
-    list.classList.toggle("active");
+listItem.forEach((list, i, arr) => {
+  list.addEventListener("click", function (e) {
+    console.log(list);
+    if (e.target.classList.contains("job1")) {
+      // list.classList.add("active");
+      arr[0].classList.add("active");
+      arr[1].classList.remove("active");
+      arr[2].classList.remove("active");
+      arr[3].classList.remove("active");
+      workHistory[0].classList.remove("hidden");
+      workHistory[1].classList.add("hidden");
+      workHistory[2].classList.add("hidden");
+      workHistory[3].classList.add("hidden");
+    } else if (e.target.classList.contains("job2")) {
+      arr[0].classList.remove("active");
+      arr[1].classList.add("active");
+      arr[2].classList.remove("active");
+      arr[3].classList.remove("active");
+      // list.classList.add("active");
+      workHistory[0].classList.add("hidden");
+      workHistory[1].classList.remove("hidden");
+      workHistory[2].classList.add("hidden");
+      workHistory[3].classList.add("hidden");
+    } else if (e.target.classList.contains("job3")) {
+      arr[0].classList.remove("active");
+      arr[1].classList.remove("active");
+      arr[2].classList.add("active");
+      arr[3].classList.remove("active");
+      workHistory[0].classList.add("hidden");
+      workHistory[1].classList.add("hidden");
+      workHistory[2].classList.remove("hidden");
+      workHistory[3].classList.add("hidden");
+    } else {
+      arr[0].classList.remove("active");
+      arr[1].classList.remove("active");
+      arr[2].classList.remove("active");
+      arr[3].classList.add("active");
+      workHistory[0].classList.add("hidden");
+      workHistory[1].classList.add("hidden");
+      workHistory[2].classList.add("hidden");
+      workHistory[3].classList.remove("hidden");
+    }
   });
 });
+
+// listItem.forEach((list, i) => {
+//   list.addEventListener("click", function (e) {
+//     if (e.target.classList.contains("job2")) {
+//       list.classList.toggle("active");
+//       workHistory[i].classList.toggle("hidden");
+//     }
+//   });
+// });
+
+// listItem.forEach((work, i) => {
+//   work.addEventListener("click", function () {
+//     work.classList.toggle("hidden");
+//   });
+// });
 
 //LOADER
 // var myVar;
